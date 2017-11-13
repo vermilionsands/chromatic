a853055a7c84049664c6aa5e3a29809f1599bae2
vermilionsands <sands.vermilion@gmail.com>
2017-11-13 21:46:22 +0100

    Add dev helper to create  hazelcast client instance.

--- dev/user.clj
+++ dev/user.clj
@@ -1,7 +1,10 @@
 (ns user
   (:require [vermilionsands.chromatic :as chromatic])
-  (:import [com.hazelcast.config Config]
+  (:import [com.hazelcast.client HazelcastClient]
+           [com.hazelcast.client.config ClientConfig]
+           [com.hazelcast.config Config]
            [com.hazelcast.core Hazelcast]))
+
 
 (defonce ^:dynamic *hazelcast* nil)
 
@@ -9,5 +12,13 @@
   (let [cfg (Config.)]
     (Hazelcast/newHazelcastInstance cfg)))
 
+(defn get-hz-client-instance [& [address]]
+  (let [cfg (ClientConfig.)]
+    (.addAddress (.getNetworkConfig cfg) (into-array [(or address "127.0.0.1")]))
+    (HazelcastClient/newHazelcastClient cfg)))
+
 (defn init! []
   (alter-var-root #'*hazelcast* (fn [_] (get-hz-instance))))
+
+(defn init-client! []
+  (alter-var-root #'*hazelcast* (fn [_] (get-hz-client-instance))))
