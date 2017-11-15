bc956fbbf508780ba4a57da34fb30f3dd7f29fb0
vermilionsands <sands.vermilion@gmail.com>
2017-11-15 22:32:34 +0100

    Add shared validator/watch.

--- src/vermilionsands/chromatic.clj
+++ src/vermilionsands/chromatic.clj
@@ -1,6 +1,17 @@
 (ns vermilionsands.chromatic
-  (:import [com.hazelcast.core IAtomicReference HazelcastInstance ITopic MessageListener]
+  (:import [com.hazelcast.core IAtomicReference HazelcastInstance ITopic MessageListener IFunction]
            [clojure.lang IDeref IAtom IRef IReference IMeta]))
+
+(deftype HazelcastFn [f]
+  IFunction
+  (apply [_ x] (f x)))
+
+(defprotocol DistributedAtom
+  (set-shared-validator [_ f])
+  (get-shared-validator [_])
+  (add-shared-watch [_ k f])
+  (remove-shared-watch [_ k])
+  (get-shared-watches [_]))
 
 (defn- validate [f x]
   (try
@@ -19,19 +30,24 @@
         f (if rest (list 'apply f) (list f))]
     `(let [old-val# (deref ~this)
            new-val# (~@f old-val# ~@args)]
-       (validate ~'local-validator new-val#)
+       (validate (:validator @~'local-ctx) new-val#)
+       (validate (:validator (.get ~'shared-ctx)) new-val#)
        (if (.compareAndSet ~'state old-val# new-val#)
          (do
            (notify ~this old-val# new-val#)
            new-val#)
          (recur ~f ~@args)))))
 
-(deftype ValueBasedHazelcastAtom
-  [^IAtomicReference state
-   ^ITopic notification-topic
-   ^:unsynchronized-mutable local-meta-map
-   ^:unsynchronized-mutable local-validator
-   ^:volatile-mutable local-watches]
+(defn- assoc-shared-validator-fn [f]
+  (->HazelcastFn #(assoc % :validator f)))
+
+(defn- assoc-shared-watch-fn [k f]
+  (->HazelcastFn #(update-in % [:watches k] f)))
+
+(defn- remove-shared-watch-fn [k]
+  (->HazelcastFn #(update % :watches dissoc k)))
+
+(deftype ValueBasedHazelcastAtom [state notification-topic shared-ctx local-ctx]
   IAtom
   (swap [this f]
     (swap* this f))
@@ -46,7 +62,8 @@
     (swap* this f x y args))
 
   (compareAndSet [this old-val new-val]
-    (validate local-validator new-val)
+    (validate (:validator @local-ctx) new-val)
+    (validate (:validator (.get shared-ctx)) new-val)
     (let [ret (.compareAndSet state old-val new-val)]
       (when ret
         (notify this old-val new-val))
@@ -54,51 +71,75 @@
 
   (reset [this new-val]
     (let [old-val (deref this)]
-      (validate local-validator new-val)
+      (validate (:validator @local-ctx) new-val)
+      (validate (:validator (.get shared-ctx)) new-val)
       @(.setAsync state new-val)
       (notify this old-val new-val)
       new-val))
 
   IMeta
-  (meta [this]
-    (locking this
-      local-meta-map))
+  (meta [_]
+    (:meta @local-ctx))
 
   IReference
-  (resetMeta [this m]
-    (locking this
-      (set! local-meta-map m)))
+  (resetMeta [_ m]
+    (swap! local-ctx assoc :meta m)
+    m)
 
-  (alterMeta [this f args]
-    (locking this
-      (set! local-meta-map (apply f local-meta-map args))))
+  (alterMeta [_ f args]
+    (let [g #(apply f % args)]
+      (:meta (swap! local-ctx update :meta g))))
 
   IRef
   (setValidator [this f]
     (validate f (deref this))
-    (set! local-validator f))
+    (swap! local-ctx assoc :validator f)
+    nil)
 
-  (getValidator [_] local-validator)
+  (getValidator [_]
+    (:validator @local-ctx))
 
   (addWatch [this k f]
-    (locking this
-      (set! local-watches (assoc local-watches k f))
-      this))
+    (swap! local-ctx update :watches conj k f)
+    this)
 
   (removeWatch [this k]
-    (locking this
-      (set! local-watches (dissoc local-watches k))
-      this))
+    (swap! local-ctx update :watches dissoc k)
+    this)
 
-  (getWatches [_] local-watches)
+  (getWatches [_]
+    (:wathes @local-ctx))
 
   IDeref
-  (deref [_] (.get state)))
+  (deref [_] (.get state))
 
-(defn distributed-atom [^HazelcastInstance instance id x]
+  DistributedAtom
+  (set-shared-validator [this f]
+    (validate f (deref this))
+    (.alter ^IAtomicReference shared-ctx (assoc-shared-validator-fn f)))
+
+  (get-shared-validator [_]
+    (:validator (.get shared-ctx)))
+
+  (add-shared-watch [this k f]
+    (.alter ^IAtomicReference shared-ctx (assoc-shared-watch-fn k f))
+    this)
+
+  (remove-shared-watch [this k]
+    (.alter ^IAtomicReference shared-ctx (remove-shared-watch-fn k))
+    this)
+
+  (get-shared-watches [_]
+    (:watches (.get shared-ctx))))
+
+(defn distributed-atom
+  ""
+  [^HazelcastInstance instance id x]
   (let [state (.getAtomicReference instance id)
+        shared-ctx (.getAtomicReference instance (str id "-ctx"))
+        local-ctx (atom {})
         notification-topic (.getReliableTopic instance id)
-        atom (->ValueBasedHazelcastAtom state notification-topic nil nil {})]
+        atom (->ValueBasedHazelcastAtom state notification-topic shared-ctx local-ctx)]
     (.addMessageListener notification-topic
       (reify MessageListener
         (onMessage [_ message]
