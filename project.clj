bf4200142f920b7b8a7d9c29d6ca1ca01ccf57a3
vermilionsands <sands.vermilion@gmail.com>
2017-11-22 23:09:17 +0100

    Update project url.

--- project.clj
+++ project.clj
@@ -1,5 +1,6 @@
 (defproject chromatic "0.0.1"
   :description "Distributed atom-like reference type backed by Hazelcast"
+  :url "https://github.com/vermilionsands/chromatic"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
